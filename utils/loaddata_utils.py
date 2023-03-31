b998147323f233a3e911563b6bc2a7ad9e6a45be
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-03-31 08:31:56 -0600

    update loaddata utils to use subprocess

--- utils/loaddata_utils.py
+++ utils/loaddata_utils.py
@@ -2,9 +2,12 @@
 This file contains functions to create different LoadData csvs for specific CellProfiler pipelines
 and to edit these outputted csvs.
 """
-import os
+
+
+import subprocess
 import pathlib
 import pandas as pd
+
 
 def create_loaddata_csv(
     index_directory: pathlib.Path,
@@ -23,17 +26,24 @@ def create_loaddata_csv(
     path_to_output : pathlib.Path
         path to the `wave1_loaddata.csv' file used for generating the illumination correction functions for each channel
     """
-    command = f"pe2loaddata --index-directory {index_directory} {config_path} {path_to_output}"
-    os.system(command)
-    print(f'{path_to_output.name} is created!')
+    command = [
+        "pe2loaddata",
+        "--index-directory",
+        index_directory,
+        config_path,
+        path_to_output,
+    ]
+    subprocess.run(command, check=True)
+    print(f"{path_to_output.name} is created!")
+
 
 def create_loaddata_illum_csv(
     index_directory: pathlib.Path,
     config_path: pathlib.Path,
     path_to_output: pathlib.Path,
-    illum_directory:pathlib.Path,
-    plate_id:str,
-    illum_output_path:pathlib.Path,
+    illum_directory: pathlib.Path,
+    plate_id: str,
+    illum_output_path: pathlib.Path,
 ):
     """
     Create LoadData csv with illum correction functions for CellProfiler (used for analysis pipelines)
@@ -53,13 +63,27 @@ def create_loaddata_illum_csv(
     illum_output_path : pathlib.Path
         path to where the new csv will be created along with the name (e.g. path/to/wave1_loaddata_with_illum.csv)
     """
-    command = f"pe2loaddata --index-directory {index_directory} {config_path} {path_to_output} --illum --illum-directory {illum_directory} --plate-id {plate_id} --illum-output {illum_output_path}"
-    os.system(command)
-    print(f'{illum_output_path.name} is created!')
+    command = [
+        "pe2loaddata",
+        "--index-directory",
+        index_directory,
+        config_path,
+        path_to_output,
+        "--illum",
+        "--illum-directory",
+        illum_directory,
+        "--plate-id",
+        plate_id,
+        "--illum-output",
+        illum_output_path,
+    ]
+    subprocess.run(command, check=True)
+    print(f"{illum_output_path.name} is created!")
 
-def edit_loaddata_csv(path_to_loaddata_csv:pathlib.Path):
+
+def edit_loaddata_csv(path_to_loaddata_csv: pathlib.Path):
     """
-    This function loads in the loaddata csv and edit it to remove unnessecary rows and 
+    This function loads in the loaddata csv and edit it to remove unnessecary rows and
     correct the paths to the maximum projection images.
 
     Parameters
@@ -69,7 +93,7 @@ def edit_loaddata_csv(path_to_loaddata_csv:pathlib.Path):
     """
     loaddata_df = pd.read_csv(path_to_loaddata_csv)
 
-    # finds the last z-plane value and assigns it as a variable 
+    # finds the last z-plane value and assigns it as a variable
     # Metadata_PlaneID values are 1-3 which correlates to the file names p01-p03
     final_z = max(loaddata_df["Metadata_PlaneID"].unique())
 
