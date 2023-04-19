8d2282616222a0be7079c954dfae311af015decc
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-04-19 10:42:15 -0600

    add processing features module

---
+++ Plate_2_data/4.processing_features/processing_features_plate2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# initialize the correct shell for your machine to allow conda to work (see README for note on shell names)
+conda init bash
+# activate the main conda environment
+conda activate interstellar_data
+
+# convert all notebooks to python files into the scripts folder
+jupyter nbconvert --to python --output-dir=scripts/ *.ipynb
+
+# run the python scripts in order (from convert+merge, annotate, normalize, and feature select)
+python scripts/merge_sc_plate2.py && python scripts/annotate_sc_plate2.py && python scripts/normalize_sc_plate2.py && python scripts/feature_select_sc_plate2.py
---
+++ Plate_2_data/4.processing_features/scripts/annotate_sc_plate2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Annotate merged single cells with metadata from platemap file
+
+# ## Import libraries
+
+# In[1]:
+
+
+import pandas as pd
+import pathlib
+
+from pycytominer import annotate
+from pycytominer.cyto_utils import output
+
+import sys
+sys.path.append("../../utils")
+import sc_extraction_utils as sc_utils
+
+
+# ## Set paths and variables
+
+# In[2]:
+
+
+# load in platemap file as a pandas dataframe
+platemap_path = pathlib.Path("../../metadata/Interstellar_plate2_platemap.csv")
+platemap_df = pd.read_csv(platemap_path)
+
+# output directory for annotated file
+output_dir = pathlib.Path("./data/")
+
+
+# In[3]:
+
+
+# dictionary with each run for the cell type
+run_info_dictionary = {
+    "SHSY5Y_first_run": {
+        # path to parquet file from convert function
+        "single_cell_path": str(pathlib.Path("./data/SHSY5Y_first_run.parquet"))
+    },
+    "SHSY5Y_second_run": {
+        # path to parquet file from convert function
+        "single_cell_path": str(pathlib.Path("./data/SHSY5Y_second_run.parquet")),
+    },
+}
+
+
+# ## Annotate merged single cells
+
+# In[4]:
+
+
+for SHSY5Y_run, info in run_info_dictionary.items():
+    # load in parquet file as df to use in annotate function
+    single_cell_df = pd.read_parquet(info["single_cell_path"])
+    output_file = str(pathlib.Path(f"{output_dir}/{SHSY5Y_run}_sc.parquet"))
+    print(f"Adding annotations to merged single cells for {SHSY5Y_run}!")
+
+    # add metadata from platemap file to extracted single cell features
+    annotated_df = annotate(
+        profiles=single_cell_df,
+        platemap=platemap_df,
+        join_on=["Metadata_well_id", "Image_Metadata_Well"],
+    )
+
+    # move metadata well and single cell count to the front of the df (for easy visualization in python)
+    well_column = annotated_df.pop("Metadata_Well")
+    singlecell_column = annotated_df.pop("Metadata_number_of_singlecells")
+    # insert the column as the second index column in the dataframe
+    annotated_df.insert(1, "Metadata_Well", well_column)
+    annotated_df.insert(2, "Metadata_number_of_singlecells", singlecell_column)
+
+    # save annotated df as parquet file
+    output(
+        df=annotated_df,
+        output_filename=output_file,
+        output_type="parquet",
+    )
+    print(f"Annotations have been added to {SHSY5Y_run} and saved!")
+
+
+# In[5]:
+
+
+print(annotated_df.shape)
+annotated_df.head()
+
