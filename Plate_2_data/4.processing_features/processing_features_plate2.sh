9353f5aaeab7515deac6a509c402c94d65583aa6
MikeLippincott <1michaell2017@gmail.com>
2023-04-25 13:14:18 -0600

    merge runs with new nb

--- Plate_2_data/4.processing_features/processing_features_plate2.sh
+++ Plate_2_data/4.processing_features/processing_features_plate2.sh
@@ -9,4 +9,4 @@ conda activate interstellar_data
 jupyter nbconvert --to python --output-dir=scripts/ *.ipynb
 
 # run the python scripts in order (from convert+merge, annotate, normalize, and feature select)
-python scripts/merge_sc_plate2.py && python scripts/annotate_sc_plate2.py && python scripts/normalize_sc_plate2.py && python scripts/feature_select_sc_plate2.py
+python scripts/merge_sc_plate2.py && python scripts/merge_runs_plate2.py && python scripts/annotate_sc_plate2.py && python scripts/normalize_sc_plate2.py && python scripts/feature_select_sc_plate2.py
