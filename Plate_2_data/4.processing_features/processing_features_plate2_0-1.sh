111c14694e4545076c4a0f07a2c46d803f5a49fd
MikeLippincott <1michaell2017@gmail.com>
2024-03-18 18:48:05 -0600

    update run

--- Plate_2_data/4.processing_features/processing_features_plate2_0-1.sh
+++ Plate_2_data/4.processing_features/processing_features_plate2_0-1.sh
@@ -3,9 +3,9 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=600G
-#SBATCH --partition=amem
-#SBATCH --qos=mem
+#SBATCH --mem=250G
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
 #SBATCH --time=24:00:00
 #SBATCH --output=sample-%j.out
 
@@ -22,12 +22,9 @@ jupyter nbconvert --to python --output-dir=scripts/ *.ipynb
 # run the python scripts in order (from convert+merge, annotate, normalize, feature select, and extract image features)
 echo "Starting processing of plate 2 data"
 
-echo "Combining plate 2 data"
-python scripts/2.combine_sc_runs_plate2.py 
-echo "Normalizing plate 2 data"
-python scripts/3.normalize_sc_plate2.py 
-echo "Feature selecting plate 2 data"
-python scripts/4.feature_select_sc_plate2.py
-echo "Extracting image features from plate 2 data"
-python scripts/5.extract_image_features.py
-echo "Processing of plate 2 data complete"
+echo "Converting and merging plate 2 data"
+python scripts/0.merge_sc_plate2.py 
+echo "Annotating plate 2 data"
+python scripts/1.annotate_sc_plate2.py
+
+echo "Processing of plate 2 data steps 0-1 complete"
